e99c178c8f818f87f019a6424d28818f2d9acf8a
santoshkumarvagga <vaggasantoshkumar@gmail.com>
2020-06-12 21:57:13 +0530

    added upto 10

--- singles.py
+++ singles.py
@@ -2,7 +2,7 @@ def print_singles():
 	print('Satrting to print singles')
 	for i in range(10):
 		print(i)
-	print('Successfully printed singles')
+	print('Successfully printed singles upto 10')
 
 
 print_singles()
