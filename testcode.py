5afd83de94dd3fa2de70dfc6903069910eb28628
Santoshkumar Vagga <vaggasantoshkumar@gmail.com>
2020-06-17 21:06:37 +0530

    Create testcode.py

---
+++ testcode.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class Hey(unittest.TestCase):
+	a = 4
+	b = 5
+
+	@classmethod
+	def setUpClass(cls):
+		print("Entered into Class")
+
+	def setUp(self):
+		print("Starting Testing of a function")
+
+	def sum(self):
+		return self.a + self.b
+
+	def test_validate(self):
+		'''Testcase to test sum() function'''
+		assert self.sum() == 9, 'sum failed'
+
+	def tearDown(self):
+		print("Finsihed Testing")
+
+	@classmethod
+	def tearDownClass(cls):
+		print("Leaving the class")
+
+
+Hey()
+
+
+if __name__ == '__main__':
+	unittest.main()
